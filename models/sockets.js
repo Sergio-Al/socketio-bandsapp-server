733887624d834b0106987265f41a507308b7a291
Sergio-Al <54541807+Sergio-Al@users.noreply.github.com>
2022-03-26 00:15:28 -0400

    Adding CRUD listeners from client
    Now our server listen to events from client and execute the correspondent CRUD task

--- models/sockets.js
+++ models/sockets.js
@@ -23,6 +23,25 @@ class Sockets {
         // We updated the data, we need to re-send the new data with an event and emit
         this.io.emit("current-bands", this.bandList.getBands());
       });
+
+      // delete a band
+      socket.on("delete-band", (id) => {
+        this.bandList.removeBand(id);
+
+        this.io.emit("current-bands", this.bandList.getBands());
+      });
+
+      // update the name of the band
+      socket.on("update-band-name", ({ id, name }) => {
+        this.bandList.updateName(id, name);
+        this.io.emit("current-bands", this.bandList.getBands());
+      });
+
+      // Create a new band
+      socket.on("create-new-band", ({ name }) => {
+        this.bandList.addBand(name);
+        this.io.emit("current-bands", this.bandList.getBands());
+      });
     });
   }
 }
