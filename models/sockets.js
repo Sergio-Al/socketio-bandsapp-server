4238bef5b3e48e31d5b1b8b38e099ecf4bfedb36
Sergio-Al <54541807+Sergio-Al@users.noreply.github.com>
2022-03-25 22:26:21 -0400

    listening for vote events
    we are listening to vote event from client
    - we fixed an error on client methods

--- models/sockets.js
+++ models/sockets.js
@@ -15,6 +15,14 @@ class Sockets {
 
       // Emit to the client an event that shows all the existing bands
       socket.emit("current-bands", this.bandList.getBands());
+
+      // add a vote
+      socket.on("vote-band", (id) => {
+        this.bandList.increaseVotes(id);
+
+        // We updated the data, we need to re-send the new data with an event and emit
+        this.io.emit("current-bands", this.bandList.getBands());
+      });
     });
   }
 }
